5e4ef5199118405c3d7c93f1edd6af26a22cfb43
Shawky Gamal <shawkygamal150@gmail.com>
2025-09-26 17:45:53 +0300

    adding lifspan in start up and shutdown and making the postgres connection and db_client

--- src/main.py
+++ src/main.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
 from routes import base, data, nlp
+from contextlib import asynccontextmanager
+from helpers.config import get_settings
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup logic here
+    # await some_async_startup_function()
+    settings = get_settings()
+    postgres_conn = f"postgresql+asyncpg://{settings.POSTGRES_USERNAME}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_MAIN_DATABASE}"
+    app.db_engine = create_async_engine(postgres_conn, echo=True)
+    
+    app.db_client = sessionmaker(
+        app.db_engine, class_=AsyncSession, expire_on_commit=False
+    )
 
-app = FastAPI()
+    yield
+    # Shutdown logic here
+    # await some_async_shutdown_function()
+    app.db_engine.dispose()
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
