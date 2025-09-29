c7ab3259638eebad1735ba676216478febafb59a
shawky-gamal-22 <shawkygamal118@gmail.com>
2025-09-29 15:24:05 +0300

    adding Idexes for easy accessing and search

--- src/models/db_schemes/MyWallet/schemes/invoice.py
+++ src/models/db_schemes/MyWallet/schemes/invoice.py
@@ -1,5 +1,5 @@
 from .base import SQLAlchemyBase
-from sqlalchemy import Column, Integer, String, DateTime, func
+from sqlalchemy import Column, Integer, String, DateTime, func, Float, Index
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import ForeignKey
 
@@ -10,10 +10,19 @@ class Invoice(SQLAlchemyBase):
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     category_id = Column(Integer, ForeignKey("categories.id"), nullable=False)
-    total_price = Column(Integer, nullable=False)
+    total_price = Column(Float, nullable=False)
     description = Column(String, nullable=True)
     created_at = Column(DateTime(timezone=True),server_default=func.now(), nullable=False)
     img_path = Column(String, nullable=True)
 
     user = relationship("User", back_populates="invoices")
     category = relationship("Category", back_populates="invoices")
+
+
+
+    __table_args__ = (
+        Index('idx_user_created_at', 'user_id', 'created_at'),
+        Index('idx_category_created_at', 'category_id', 'created_at'),
+        Index('idx_user_id', 'user_id'),
+        Index('idx_created_at', 'created_at'),
+    )
