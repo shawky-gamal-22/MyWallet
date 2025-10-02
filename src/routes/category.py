d4610cb307fa2b0d38ac7bd75da95cc74254e898
shawky-gamal-22 <shawkygamal118@gmail.com>
2025-10-02 18:03:05 +0300

    making a new router for getting all categoires

--- src/routes/category.py
+++ src/routes/category.py
@@ -24,3 +24,30 @@ async def create_category(request: Request, category: CategoryRequest):
             "status": ResponseStatus.CATEGORY_ADDED_SUCCESS.value,
             "category_id": created_category.id
             }
+
+@category_router.get("/get-all-categories/")
+async def get_all_categories(request: Request):
+
+    has_records = True
+    result = []
+    page_no = 1
+    page_size = 10
+
+    category_model_instance = await CategoryModel.create_instance(request.app.db_client)
+    while has_records:
+        categories = await category_model_instance.get_all_categories(page_no= page_no, page_size= page_size)
+        if len(categories) == 0:
+            has_records = False
+        else:
+            for category in categories:
+                result.append({
+                    "id": category.id,
+                    "name": category.name,
+                    "description": category.description
+                })
+            page_no += 1
+
+    return {
+        "status": ResponseStatus.GET_ALL_CATEGORIES_SUCCESS.value,
+        "data": result
+    }
