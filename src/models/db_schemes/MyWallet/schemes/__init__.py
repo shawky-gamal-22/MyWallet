a3655bd490287cdcac62f9777fb7611bec866335
shawky-gamal-22 <shawkygamal118@gmail.com>
2025-10-04 14:28:33 +0300

    adding Income and IncomeCategory into __init__

--- src/models/db_schemes/MyWallet/schemes/__init__.py
+++ src/models/db_schemes/MyWallet/schemes/__init__.py
@@ -2,3 +2,5 @@ from .base import SQLAlchemyBase
 from .user import User
 from .category import Category
 from .invoice import Invoice
+from .income import Income
+from .income_category import IncomeCategory
