235365dac35516526188f032f12954da4d0391cd
shawky-gamal-22 <shawkygamal118@gmail.com>
2025-10-08 19:54:09 +0300

    adding IncomeModel and IncomeCategoryModel

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,3 +2,5 @@ from .UserModel import UserModel
 from .CategoryModel import CategoryModel
 from .InvoiceModel import InvoiceModel
 from .BaseDataModel import BaseDataModel
+from .IncomeModel import IncomeModel
+from .IncomeCategoryModel import IncomeCategoryModel
