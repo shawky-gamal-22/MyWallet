af3c16288dc8527ce59f7bda7ab81baaa07502d0
shawky-gamal-22 <shawkygamal118@gmail.com>
2025-10-10 16:57:08 +0300

    adding IncomeCategoryController

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -2,3 +2,4 @@ from .BaseController import BaseController
 from .NLPDataController import NLPDataController
 from .DataController import DataController
 from .IncomeController import IncomeController
+from .IncomeCategoryController import IncomeCategoryController
