da8fc75c0894925997f7e54fd9bcb3a24a9fa5c0
shawky-gamal-22 <shawkygamal118@gmail.com>
2025-10-10 16:59:57 +0300

    import DeleteRequest and IncomeCategoryRequest

--- src/routes/schemes/__init__.py
+++ src/routes/schemes/__init__.py
@@ -1,4 +1,5 @@
 from .user import UserRequest
 from .category import CategoryRequest
 from .invoice import InvoiceRequest, DateRangeRequest
-from .income import IncomeRequest
+from .income import IncomeRequest, DeleteRequest
+from .income_category import IncomeCategoryRequest
