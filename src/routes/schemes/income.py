279f7fc7b31b7842ffd47e407ff68e57fa8562cb
shawky-gamal-22 <shawkygamal118@gmail.com>
2025-10-11 15:29:07 +0300

    adding two new pydantic classes for income routes UpdateRequest and UpdateNextDueDateRequest

--- src/routes/schemes/income.py
+++ src/routes/schemes/income.py
@@ -13,3 +13,20 @@ class IncomeRequest(BaseModel):
 
 class DeleteRequest(BaseModel):
     income_id: int
+
+
+class UpdateRequest(BaseModel):
+    income_id : int
+    category_name: str | None = None
+    source_name: str| None = None
+    amount: float | None = None
+    is_recurring: bool| None = None
+
+    description: str | None = None
+    recurrence_interval: str | None = None
+    next_due_date: str | None = None
+
+
+class UpdateNextDueDateRequest(BaseModel):
+    income_id: int
+    next_due_date: str
