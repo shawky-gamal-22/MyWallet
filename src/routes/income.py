df7eac1066f2623acd549e959499b8deb61f76bf
shawky-gamal-22 <shawkygamal118@gmail.com>
2025-10-11 15:28:16 +0300

    adding two new roytes update income and update next_due_date

--- src/routes/income.py
+++ src/routes/income.py
@@ -1,7 +1,7 @@
 from fastapi import  APIRouter, Request
 from controllers import IncomeController
 from models.enums import ResponseStatus
-from .schemes import IncomeRequest, DeleteRequest
+from .schemes import IncomeRequest, DeleteRequest, UpdateRequest, UpdateNextDueDateRequest
 from datetime import datetime, timezone
 
 
@@ -82,6 +82,90 @@ async def delete_income_by_id(request: Request, user_id: int ,income_id:DeleteRe
             "next_due_date": deleted_income.next_due_date
 
         }
+
+    except Exception as e:
+        return {
+            "status": ResponseStatus.ERROR.value,
+            "message": str(e)
+        }
+
+
+@income_router.post("/update_income/{user_id}")
+async def update_income(request: Request, user_id: int, update_request: UpdateRequest):
+
+    try:
+        income_controller = await IncomeController.create_instance(db_client= request.app.db_client)
+
+        updated_income = await income_controller.update_income(
+            user_id= user_id,
+            income_id= update_request.income_id,
+            category_name= update_request.category_name,
+            source_name = update_request.source_name,
+            amount = update_request.amount,
+            is_recurring = update_request.is_recurring,
+            description = update_request.description,
+            recurrence_interval = update_request.recurrence_interval,
+            next_due_date = update_request.next_due_date
+        )
+
+        if update_income is None:
+            return{
+                "signal": ResponseStatus.INCOME_ID_DOES_NOT_EXISTS.value
+            }
+        
+        return {
+            "signal": ResponseStatus.UPDATED_INCOME_SUCCESSFULLY.value,
+            "Income_id": updated_income.id,
+            "user_id": updated_income.user_id,
+            "category_id": updated_income.category_id,
+            "source_name": updated_income.source_name,
+            "amount": updated_income.amount,
+            "is_recurring":updated_income.is_recurring,
+            "description": updated_income.description,
+            "recurrence_interval": updated_income.recurrence_interval,
+            "next_due_date": updated_income.next_due_date
+        }
+
+    except Exception as e:
+        return {
+            "status": ResponseStatus.ERROR.value,
+            "message": str(e)
+        }
+
+
+
+@income_router.post("/update_next_due_date/{user_id}")
+async def update_next_due_date(request: Request, user_id: int, update_next_due_date_request: UpdateNextDueDateRequest):
+
+    try:
+        income_controller = await IncomeController.create_instance(db_client= request.app.db_client)
+
+
+        updated_income = await income_controller.update_next_due_date(
+            user_id= user_id,
+            income_id= update_next_due_date_request.income_id,
+            next_due_date= update_next_due_date_request.next_due_date
+        )
+
+        if update_income is None:
+            return {
+                "signal": ResponseStatus.INCOME_ID_DOES_NOT_EXISTS.value
+            }
+        
+        return {
+            "signal": ResponseStatus.UPDATED_INCOME_SUCCESSFULLY.value,
+            "Income_id": updated_income.id,
+            "user_id": updated_income.user_id,
+            "category_id": updated_income.category_id,
+            "source_name": updated_income.source_name,
+            "amount": updated_income.amount,
+            "is_recurring":updated_income.is_recurring,
+            "description": updated_income.description,
+            "recurrence_interval": updated_income.recurrence_interval,
+            "next_due_date": updated_income.next_due_date
+        }
+
+
 
     except Exception as e:
         return {
