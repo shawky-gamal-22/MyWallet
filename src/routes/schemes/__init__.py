719d3d574895854e0487c366179fc2e1f471e6ae
shawky-gamal-22 <shawkygamal118@gmail.com>
2025-10-11 15:28:38 +0300

    adding UpdateRequest and UpdateNextDueDateRequest into __init__ for easy importing

--- src/routes/schemes/__init__.py
+++ src/routes/schemes/__init__.py
@@ -1,5 +1,5 @@
 from .user import UserRequest
 from .category import CategoryRequest
 from .invoice import InvoiceRequest, DateRangeRequest
-from .income import IncomeRequest, DeleteRequest
+from .income import IncomeRequest, DeleteRequest, UpdateRequest, UpdateNextDueDateRequest
 from .income_category import IncomeCategoryRequest
