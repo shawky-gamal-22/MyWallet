83d5b3640bef6253210a027cf85f0372022c2bad
shawky-gamal-22 <shawkygamal118@gmail.com>
2025-10-13 15:28:03 +0300

    adding UserBalanceModel for easy importing

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,3 +4,4 @@ from .InvoiceModel import InvoiceModel
 from .BaseDataModel import BaseDataModel
 from .IncomeModel import IncomeModel
 from .IncomeCategoryModel import IncomeCategoryModel
+from .UserBalanceModel import UserBalanceModel
