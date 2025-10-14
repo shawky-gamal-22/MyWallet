1efc528e51f5af64b14b3d6e005f651e8d5b31a0
shawky-gamal-22 <shawkygamal118@gmail.com>
2025-10-14 11:31:01 +0300

    adding UserController into __init__ for easy importing

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -3,3 +3,4 @@ from .NLPDataController import NLPDataController
 from .DataController import DataController
 from .IncomeController import IncomeController
 from .IncomeCategoryController import IncomeCategoryController
+from .UserController import UserController
