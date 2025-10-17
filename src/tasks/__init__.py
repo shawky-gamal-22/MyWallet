a5be92ab7ad17bdabef2a3629a0cc452cd59b225
shawky-gamal-22 <shawkygamal118@gmail.com>
2025-10-17 19:33:53 +0300

    adding the recurring invoices task

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -1,4 +1,4 @@
 from .update_recurring_income import update_recurring_income
+from .update_recurring_invoices import update_recurring_invoices
 
-
-__all__ = ['update_recurring_income']
+__all__ = ['update_recurring_income', 'update_recurring_invoices']
