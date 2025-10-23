a48883e8886b6b48fb98533bef193cc4ed52d994
shawky-gamal-22 <shawkygamal118@gmail.com>
2025-10-23 14:28:54 +0300

    check if check_relevance node work well or not.

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Depends, APIRouter, UploadFile, File
+from fastapi import FastAPI, Depends, APIRouter, UploadFile, File, Request
 from helpers.config import get_settings
 from stores.OCR import Pytesseract, MistralOCR
+from agents.SQLAgent.utils.nodes import check_relevance
 
 data_router = APIRouter()
 
@@ -31,3 +32,7 @@ async def extract_text(file: UploadFile = File(...), app_settings=Depends(get_se
         "extracted_text": extracted_text
     }
 
+@data_router.get("/try_relevance")
+async def relevance(request: Request):
+
+    return await check_relevance(engine= request.app.db_engine)
