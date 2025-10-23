6d60354d49df92fb321715e153c0d3e2a4d0c0c6
shawky-gamal-22 <shawkygamal118@gmail.com>
2025-10-23 14:24:39 +0300

    creating AgentState the main or the global state for the SQLAgent

---
+++ src/agents/SQLAgent/utils/state.py
@@ -0,0 +1,16 @@
+from typing_extensions import TypedDict
+from pydantic import BaseModel, Field
+
+
+class AgentState(BaseModel):
+
+    question: str
+    sql_query: str
+    query_result: str
+    query_rows: list
+    user_id: int
+    user_name: str
+    attempts: int 
+    relevence: str
+    sql_error: bool
+  
