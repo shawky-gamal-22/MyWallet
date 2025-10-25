4d20e3016aa9ca0eb6a82af149d98f19d9988e10
shawky-gamal-22 <shawkygamal118@gmail.com>
2025-10-25 19:29:25 +0300

    adding execute_sql for easy importing

--- src/agents/SQLAgent/utils/nodes/__init__.py
+++ src/agents/SQLAgent/utils/nodes/__init__.py
@@ -1,3 +1,4 @@
 from .get_current_user import get_current_user
 from .check_relevence import check_relevance, relevance_router
 from .convert_to_sql import convert_to_sql
+from .execute_sql import execute_sql
