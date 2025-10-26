3dbd29a6667e704c8d19adff6622c4f051a5245f
shawky-gamal-22 <shawkygamal118@gmail.com>
2025-10-26 10:46:13 +0300

    adding the return_state execute_sql_router end_max_iterations check_attempts_router
    nodes

--- src/agents/SQLAgent/utils/nodes/__init__.py
+++ src/agents/SQLAgent/utils/nodes/__init__.py
@@ -2,3 +2,5 @@ from .get_current_user import get_current_user
 from .check_relevence import check_relevance, relevance_router
 from .convert_to_sql import convert_to_sql
 from .execute_sql import execute_sql
+from .regenerate_question import regenerate_question, execute_sql_router, end_max_iterations, check_attempts_router
+from .return_state import return_state
